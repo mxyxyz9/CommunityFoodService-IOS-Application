b40556ae0a09f4c9a53ddc33282d28f8b9bb349e
Rushil <121330302+mxyxyz9@users.noreply.github.com>
2025-02-19 07:42:40 +0530

    Initial Commit

---
+++ food/foodApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodApp.swift
+//  food
+//
+//  Created by Pala Rushil on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct foodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foodTests/foodTests.swift
@@ -0,0 +1,17 @@
+//
+//  foodTests.swift
+//  foodTests
+//
+//  Created by Pala Rushil on 2/19/25.
+//
+
+import Testing
+@testable import food
+
+struct foodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
